0ff5b232f284f99a8f4ff2d4d8b932d526693294
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-02-21 15:16:23 +0100

    Route til postmapping tilføjet

--- src/main/java/org/example/animalversion1/controller/dogRestController.java
+++ src/main/java/org/example/animalversion1/controller/dogRestController.java
@@ -3,8 +3,8 @@ package org.example.animalversion1.controller;
 import org.example.animalversion1.model.Dog;
 import org.example.animalversion1.repositories.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,15 +14,25 @@ public class dogRestController {
     @Autowired
     DogRepository dogRepository;
 
+    //Get hello-message
     @GetMapping("/")
     public String hello() {
         return "This is my dog project";
     }
 
+    //Get all dogs in database
     @GetMapping("/dogs")
     public List<Dog> getAllDogs() {
         return dogRepository.findAll();
     }
+
+    @PostMapping("/dog")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Dog postDog (@RequestBody Dog dog) {
+        System.out.println(dog);
+        return dogRepository.save(dog);
+    }
+
 
 
 }
